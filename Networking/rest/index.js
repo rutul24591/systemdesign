b54b27b2eeb808354ddad3b38a97bc16ad66bcdb
Rutul Amin <rutulamin.ra@gmail.com>
2025-03-24 12:38:52 +0530

    REST API implmentation

--- Networking/rest/index.js
+++ Networking/rest/index.js
@@ -1,18 +1,79 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
-const PORT = 3001;  // Port number for the server to listen on
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-
-// Define the routes
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.all('/', (req, res) => {
+  // console.log('Request > ', req);
+  // console.log('Response >', res);
+  res.send(`I'm up!`);
 });
 
+const todos = [{
+  id: '1',
+  title: 'Task 1',
+  completed: false
+}, {
+  id: '2',
+  title: 'Task 2',
+  completed: true
+}];
+
+// READ
+app.get('/todos', (req, res) => {
+  res.json(todos);
+})
+
+// CREATE
+app.post('/todos', (req, res) => {
+  const newTodo = req.body;
+  todos.push(newTodo);
+  res.status(201).json({
+    message: 'New Todo Added!'
+  });
+});
+
+// UPDATE
+app.put('/todos/:id', (req, res) => {
+  const newTodoData = req.body;
+  const todoParamId = req.params.id;
+  const todoIndex = todos.findIndex(td => td.id === todoParamId);
+
+  if (todoIndex !== -1) {
+    todos[todoIndex] = {
+      id: todoParamId,
+      ...newTodoData,
+    }
+    res.json({
+      message: 'Todo updated successfully!'
+    });
+  } else {
+    res.status(400).json({
+      message: 'Todo Id does not exist!'
+    });
+  }
+
+})
+
+// DELETE
+app.delete('/todos/:id', (req, res) => {
+  const todoParamId = req.params.id;
+  const todoIndex = todos.findIndex(td => td.id === todoParamId);
+
+  if (todoIndex !== -1) {
+    todos.splice(todoIndex, 1);
+  }
+
+  res.json({
+    message: 'Todo deleted successfully!'
+  });
+
+})
+
+
+const PORT = 5111;
 app.listen(PORT, () => {
-  console.log(`REST server is listening at 127.0.0.1:${PORT}`);
+  console.log(`Server is running at port ${PORT}`);
 });

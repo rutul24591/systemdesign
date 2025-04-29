63285434de6a6687df4cd355a264d4b3ac4dc773
Rutul Amin <rutulamin.ra@gmail.com>
2025-04-29 09:56:43 +0530

    Initial commit for video chat

---
+++ LowLevelSystemDesign/app/src/components/YoutubeLiveChat/AddChat.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddChat = () => {
+  return (
+    <div>AddChat</div>
+  )
+}
+
+export default AddChat;
---
+++ LowLevelSystemDesign/app/src/components/YoutubeLiveChat/Video.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Video = () => {
+  return (
+    <div>
+      <iframe
+        width='1302'
+        height='776'
+        src='https://www.youtube.com/embed/RbFEM1denFU'
+        title='Talking Tom part 15 live gameplay #live #funnygames'
+        frameborder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        referrerpolicy='strict-origin-when-cross-origin'
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default Video;
